ad1e40647fb6646f468072543c398d24a3d85130
AlexisNava <brian.alexis.nava.arreola@gmail.com>
2020-12-22 20:25:45 -0600

    feat: Load the environment variables from the .env file.

--- main.go
+++ main.go
@@ -1,3 +1,19 @@
 package main
 
-func main() {}
+import (
+	"log"
+
+	"github.com/alex-airbnb/golang_spike_project/database"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	db := database.SetUp()
+
+}

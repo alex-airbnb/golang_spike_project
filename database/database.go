60cef7263e5ba831596b233d78dd99dfa95a6a7c
AlexisNava <brian.alexis.nava.arreola@gmail.com>
2020-12-28 19:50:05 -0600

    feat: Add database package.

--- database/database.go
+++ database/database.go
@@ -2,12 +2,13 @@ package database
 
 import (
 	"log"
+	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 
-	"github.com/alex-airbnb/golang_spike_project/config"
 	"github.com/alex-airbnb/golang_spike_project/model"
+	"github.com/joho/godotenv"
 )
 
 func migrate(db *gorm.DB) {
@@ -16,7 +17,13 @@ func migrate(db *gorm.DB) {
 
 // SetUp Set up the connection with the Postgres DB.
 func SetUp() *gorm.DB {
-	db, err := gorm.Open(postgres.Open(config.DBURL), &gorm.Config{})
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	db, err := gorm.Open(postgres.Open(os.Getenv("DB_URL")), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal("Error to connect to the database")
--- main.go
+++ main.go
@@ -1,19 +1,9 @@
 package main
 
 import (
-	"log"
-
 	"github.com/alex-airbnb/golang_spike_project/database"
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load()
-
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
-	db := database.SetUp()
-
+	database.SetUp()
 }

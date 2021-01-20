974dfe080e0a5d7190f4867ac7e5b5e078c9a594
AlexisNava <brian.alexis.nava.arreola@gmail.com>
2021-01-19 22:06:25 -0600

    feat: Add CreateArticle handler.

---
+++ router/article.go
@@ -0,0 +1,34 @@
+package router
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/alex-airbnb/golang_spike_project/adapter"
+	"github.com/alex-airbnb/golang_spike_project/database"
+	"github.com/alex-airbnb/golang_spike_project/model"
+	"github.com/alex-airbnb/golang_spike_project/module"
+)
+
+// CreateArticleHandler Handle the a POST request and create new article
+func CreateArticleHandler(writer http.ResponseWriter, request *http.Request) {
+	var article model.Article
+
+	adapter := adapter.Postgres{DB: database.SetUp()}
+
+	err := json.NewDecoder(request.Body).Decode(&article)
+
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusBadRequest)
+	}
+
+	createdArticle, err := module.CreateArticle(&article, &adapter)
+
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusBadRequest)
+	}
+
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusCreated)
+	json.NewEncoder(writer).Encode(createdArticle)
+}
